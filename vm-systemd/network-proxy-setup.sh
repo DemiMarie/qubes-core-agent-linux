9aab3b46072b4e65a95b900a31c39260ff455829
Demi Marie Obenour <athena@invisiblethingslab.com>
2020-12-28 23:28:54 -0500

    Mass replace: ‘/var/run’ → ‘/run’
    The former is a symlink to the latter and is longer.  No point in using
    it.

--- vm-systemd/network-proxy-setup.sh
+++ vm-systemd/network-proxy-setup.sh
@@ -20,8 +20,8 @@ if [ "x$network" != "x" ]; then
     primary_dns=$(qubesdb-read /qubes-netvm-primary-dns 2>/dev/null || echo "$gateway")
     secondary_dns=$(qubesdb-read /qubes-netvm-secondary-dns)
     modprobe netbk 2> /dev/null || modprobe xen-netback || "${modprobe_fail_cmd}"
-    echo "NS1=$primary_dns" > /var/run/qubes/qubes-ns
-    echo "NS2=$secondary_dns" >> /var/run/qubes/qubes-ns
+    echo "NS1=$primary_dns" > /run/qubes/qubes-ns
+    echo "NS2=$secondary_dns" >> /run/qubes/qubes-ns
     /usr/lib/qubes/qubes-setup-dnat-to-ns
     echo "1" > /proc/sys/net/ipv4/ip_forward
     # enable also IPv6 forwarding, if IPv6 is enabled
--- vm-systemd/qubes-sysinit.sh
+++ vm-systemd/qubes-sysinit.sh
@@ -17,33 +17,33 @@ while [ ! -e /dev/xen/xenbus ]; do
 done
 
 mkdir -p /var/run/qubes
-chgrp qubes /var/run/qubes
-chmod 0775 /var/run/qubes
+chgrp qubes /run/qubes
+chmod 0775 /run/qubes
 mkdir -p /var/run/qubes-service
-mkdir -p /var/run/xen-hotplug
+mkdir -p /run/xen-hotplug
 
 # Set default services depending on VM type
-is_appvm && DEFAULT_ENABLED=$DEFAULT_ENABLED_APPVM && touch /var/run/qubes/this-is-appvm
-is_netvm && DEFAULT_ENABLED=$DEFAULT_ENABLED_NETVM && touch /var/run/qubes/this-is-netvm
-is_proxyvm && DEFAULT_ENABLED=$DEFAULT_ENABLED_PROXYVM && touch /var/run/qubes/this-is-proxyvm
-is_templatevm && DEFAULT_ENABLED=$DEFAULT_ENABLED_TEMPLATEVM && touch /var/run/qubes/this-is-templatevm
+is_appvm && DEFAULT_ENABLED=$DEFAULT_ENABLED_APPVM && touch /run/qubes/this-is-appvm
+is_netvm && DEFAULT_ENABLED=$DEFAULT_ENABLED_NETVM && touch /run/qubes/this-is-netvm
+is_proxyvm && DEFAULT_ENABLED=$DEFAULT_ENABLED_PROXYVM && touch /run/qubes/this-is-proxyvm
+is_templatevm && DEFAULT_ENABLED=$DEFAULT_ENABLED_TEMPLATEVM && touch /run/qubes/this-is-templatevm
 
 # Enable default services
 for srv in $DEFAULT_ENABLED; do
-    touch "/var/run/qubes-service/$srv"
+    touch "/run/qubes-service/$srv"
 done
 
 # Enable services
 for srv in $(qubesdb-multiread /qubes-service/ 2>/dev/null |grep ' = 1'|cut -f 1 -d ' '); do
-    touch "/var/run/qubes-service/$srv"
+    touch "/run/qubes-service/$srv"
 done
 
 # Disable services
 for srv in $(qubesdb-multiread /qubes-service/ 2>/dev/null |grep ' = 0'|cut -f 1 -d ' '); do
-    rm -f "/var/run/qubes-service/$srv"
+    rm -f "/run/qubes-service/$srv"
 done
 
 # Prepare environment for other services
-echo > /var/run/qubes-service-environment
+echo > /run/qubes-service-environment
 
 exit 0
